78d44bfd01ac8dbd81346d9f5202b07bbbf4b00d
Alex <akomatoznik@gmail.com>
2025-10-22 12:39:15 +0300

    lesson 3

--- src/components/ToDo/ToDo.tsx
+++ src/components/ToDo/ToDo.tsx
@@ -1,15 +1,42 @@
-import './ToDo.scss'
+import "./ToDo.scss"
 import { ToDoItem } from "./ToDoItem/ToDoItem"
+import { todo } from "../../models/todo-item"
 
-export const ToDo = () => {
+export const ToDo = (props: { todos: todo[], updateTodo: Function, deleteTodo: Function }) => {
+  const checkedList = () => {
+    return props.todos
+      .filter((item) => !item.isDone)
+      .map((item, idx) => {
+        return (
+          <ToDoItem
+            item={item}
+            key={idx}
+            updateTodo={props.updateTodo}
+            deleteTodo={props.deleteTodo}
+          />
+        )
+      })
+  }
+
+  const unCheckedList = () => {
+    return props.todos
+      .filter((item) => item.isDone)
+      .map((item, idx) => {
+        return (
+          <ToDoItem
+            item={item}
+            key={idx}
+            updateTodo={props.updateTodo}
+            deleteTodo={props.deleteTodo}
+          />
+        )
+      })
+  }
+
   return (
     <div className="todo-container">
-      <ul className="todo-list failed">
-        <ToDoItem />
-      </ul>
-      <ul className="todo-list completed">
-        <ToDoItem />
-      </ul>
+      <ul className="todo-list failed">{checkedList()}</ul>
+      <ul className="todo-list completed">{unCheckedList()}</ul>
     </div>
   )
 }
--- src/components/ToDo/ToDoItem/ToDoItem.tsx
+++ src/components/ToDo/ToDoItem/ToDoItem.tsx
@@ -1,12 +1,23 @@
 import './ToDoItem.scss'
+import { todo } from '../../../models/todo-item'
 
-export const ToDoItem = () => {
+export const ToDoItem = (props: {
+  item: todo
+  updateTodo: Function
+  deleteTodo: Function
+}) => {
   return (
     <li className="todo-list-item__wrapper">
-      <span>Первая задача</span>
+      <span>{props.item.text}</span>
       <div className="todo-list-item__buttons">
-        <button className="btn-trash"></button>
-        <button className="btn-check"></button>
+        <button
+          className="btn-trash"
+          onClick={() => props.deleteTodo(props.item)}
+        ></button>
+        <button
+          className={props.item.isDone ? "btn-check" : "btn-uncheck"}
+          onClick={() => props.updateTodo(props.item)}
+        ></button>
       </div>
     </li>
   )
